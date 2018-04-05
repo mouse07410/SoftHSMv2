763602b098c4f4003990832f147efd16b60eaa90
Mouse <mouse008@gmail.com>
2018-04-04 23:00:53 -0400

    Add auto-configurator and builder script. It can also be used with "git bisect run softhsm-build.sh" to find bad commits

---
+++ softhsm-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+#DEBUG="-g"
+
+make distclean
+bash ./augogen.sh
+./configure --prefix=/opt/local -enable-64bit --with-openssl=/opt/local --with-botan=/opt/local --with-sqlite3 --disable-eddsa CC=clang CPPUNIT_CFLAGS="-I/opt/local/include" CFLAGS="-maes -mpclmul -mrdrnd -msse2 -mssse3 -msse4.2 -mtune=native -Os -Ofast ${DEBUG}" LDFLAGS="${DEBUG} -L/opt/local/lib" CPPFLAGS="-I/opt/local/include" CXX=clang++ CXXFLAGS="-maes -mpclmul -mrdrnd -msse2 -mssse3 -msse4.2 -mtune=native -Os -Ofast -std=c++11 ${DEBUG}"
+# Make sure configuration succeeded
+if [ $? != 0 ]; then
+   echo "SoftHSMv2 configure script failed!"
+   exit 1
+fi
+# Build, check, and install SoftHSMv2
+make -j4 all && make check && sudo make install && sudo chown -R uri *
+if [ $? != 0 ]; then
+   echo "SoftHSMv2 build, check, or install failed!"
+   exit 1
+fi
+
+# If we're here, SoftHSMv2 compiled, passed its tests, and installed OK
+echo "SoftHSMv2 installation completed"
+exit 0
+
+# Perhaps as yet another strong test - try libp11 tests that use SoftHSMv2...
+cd ../libp11 && make check
